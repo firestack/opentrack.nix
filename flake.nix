49cbd901274a96bd01e206b6d958c7ff0b2b8c94
Kayla Firestack <git.dev@kaylafire.me>
2023-04-16 20:13:27 -0400

    flake: add package, add aruco

--- flake.nix
+++ flake.nix
@@ -5,8 +5,7 @@
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = { self, flake-utils, devshell, nixpkgs }:
-    flake-utils.lib.eachDefaultSystem (system: {
-      devShell =
+    flake-utils.lib.eachDefaultSystem (system: 
         let
           pkgs = import nixpkgs {
             inherit system;
@@ -14,8 +13,15 @@
             overlays = [ devshell.overlays.default ];
           };
         in
+    {
+      devShell =
         pkgs.devshell.mkShell {
           imports = [ (pkgs.devshell.importTOML ./devshell.toml) ];
         };
+
+      packages = rec {
+        opentrack = pkgs.libsForQt5.callPackage ./default.nix {};
+        default = opentrack;
+      };
     });
 }
